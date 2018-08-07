df61c1da262b408ee6abbaad96752705a6e49cba
manmany2017 <manmany@manmany-a01.vmware.com>
2018-08-07 20:00:45 +0800

    updated testDemo

--- src/test/java/com/man/test/TestDemo.java
+++ src/test/java/com/man/test/TestDemo.java
@@ -1,11 +1,19 @@
 package com.man.test;
+import java.awt.Choice;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class TestDemo {
 	@Test
 	public void TestDemo() {
+		String osType = System.getProperty("os.name");
+		if(osType.equalsIgnoreCase("Windows")) {
+			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+		}
+
 		WebDriver driver = new FirefoxDriver();
 		String urlAddress = "http://www.baidu.com";
 		driver.get(urlAddress);
